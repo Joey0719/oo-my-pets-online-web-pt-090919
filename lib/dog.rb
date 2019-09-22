9a4015a9a872a998cfdae3a8e9cea7945b25cb06
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-09-22 16:05:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,18 @@
 class Dog
+  # code goes here
   attr_accessor :mood, :owner
-  attr_reader :name 
-  
-  @@all 
-  
-  def initialize(name, owner)
-    @name = name 
-    @mood = "nervous"
-    @owner = owner 
-    @@all << self 
-end
+  attr_reader :name
 
-  def self.all 
-    @@all 
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
